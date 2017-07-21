895029de745354b3025527307bf5d37aa16a1070
mariocq <mfm@vip.qq.com>
2017-07-21 16:00:13 +0800

    调整通知样式

--- rest/message.py
+++ rest/message.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from jinja2 import Environment, PackageLoader
+import time
 
 env = Environment(loader=PackageLoader('templates', ''))
 
@@ -15,16 +16,15 @@ def get_text_card_msg(title, description, url):
 def pr_msg(repo, payload):
     pr = {
         'html_url': payload['pull_request']['html_url'],
-        'assignee': payload['pull_request']['assignee']['login'],
         'title': payload['pull_request']['title'],
+        'time': time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),
         'action': payload['action'],
         'user_name': payload['pull_request']['user']['login'],
         'user_html': payload['pull_request']['user']['html_url']
     }
     template = env.get_template('pr.txt')
     return get_text_card_msg(
-        title=pr['title'],
+        title='[PR] ' + pr['title'],
         description=template.render(repo=repo, pr=pr),
         url=pr['html_url']
     )
-
